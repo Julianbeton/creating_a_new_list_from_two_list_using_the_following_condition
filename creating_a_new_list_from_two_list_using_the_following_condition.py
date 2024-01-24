94a5168dd101a803c572a03190c2bfccd0384357
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-24 18:18:26 +0800

    uploading function

--- creating_a_new_list_from_two_list_using_the_following_condition.py
+++ creating_a_new_list_from_two_list_using_the_following_condition.py
@@ -11,8 +11,17 @@
 # Expected Output:
 # result list: [25, 35, 40, 60, 90]
 
+def combine_odd_list(list1, list2):
+    combine_list = []
 
+    for number in list1:
+        if number % 2 == 0:
+            combine_list.append(number)
 
+    for number in list2:
+        if number % 2 == 0:
+            combine_list.append(number)
+    return combine_list
 
 list1 = [10, 20, 25, 30, 35]
 list2 = [40, 45, 60, 75, 90]
